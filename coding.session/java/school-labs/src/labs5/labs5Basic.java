b9e30bc4b3bdec1763df88498f5e8da49c58bc0c
Xulio <76985061+RikuRedfox@users.noreply.github.com>
2022-12-07 18:49:49 +0800

    codes commited

--- coding.session/java/school-labs/src/labs5/labs5Basic.java
+++ coding.session/java/school-labs/src/labs5/labs5Basic.java
@@ -6,50 +6,51 @@ public class labs5Basic {
 
     static Scanner sc = new Scanner(System.in);
 
-    public class Cipher {
-        static public String text = new String();
-        static public int key;
-        static public char[] chars;
+    static public String text = new String();
+    static public int key;
+    static public char[] chars;
 
-        public static void main(String[] args) {
-            Encrypt();
+    public static void main(String[] args) {
+        Encrypt();
 
-            sc.nextLine();
+        sc.nextLine();
 
-            Decrypt();
+        Decrypt();
+    }
+
+    public static void Encrypt() {
+        System.out.print("\n\nEnter an text to Encrypt : ");
+        text = sc.nextLine();
+        text.toUpperCase();
+        System.out.print("Enter shift key : ");
+        key = sc.nextInt();
+
+        chars = text.toCharArray();
+        System.out.print("\nEncrypted text : ");
+
+        // Encrypting method based on ASCII table
+        for (char c : chars) {
+            c += key;
+            System.out.print(c);
         }
 
-        public static void Encrypt() {
-            System.out.print("\n\nEnter an text to Encrypt : ");
-            text = sc.nextLine();
-            System.out.print("Enter shift key : ");
-            key = sc.nextInt();
+    }
 
-            chars = text.toCharArray();
-            System.out.print("\nEncrypted text : ");
+    public static void Decrypt() {
+        System.out.print("\n\nEnter an text to Decrypt : ");
+        text = sc.nextLine();
+        text.toUpperCase();
+        System.out.print("Enter shift key : ");
+        key = sc.nextInt();
 
-            // Encrypting method based on ASCII table
-            for (char c : chars) {
-                c += key;
-                System.out.print(c);
-            }
+        chars = text.toCharArray();
+        System.out.print("\nDecrypted text : ");
 
-        }
-
-        public static void Decrypt() {
-            System.out.print("\n\nEnter an text to Decrypt : ");
-            text = sc.nextLine();
-            System.out.print("Enter shift key : ");
-            key = sc.nextInt();
-
-            chars = text.toCharArray();
-            System.out.print("\nDecrypted text : ");
-
-            // Decrypting method based on ASCII table
-            for (char c : chars) {
-                c -= key;
-                System.out.print(c);
-            }
+        // Decrypting method based on ASCII table
+        for (char c : chars) {
+            c -= key;
+            System.out.print(c);
         }
     }
+
 }

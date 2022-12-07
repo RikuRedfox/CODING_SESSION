d797c56872af4581eaba0127f805d4a3f0947b49
Xulio <76985061+RikuRedfox@users.noreply.github.com>
2022-12-07 11:18:57 +0800

    codes commited

--- coding.session/java/school-labs/src/labs5/labs5.java
+++ coding.session/java/school-labs/src/labs5/labs5.java
@@ -8,8 +8,7 @@ public class labs5 {
 
     public class Cipher {
         public static final String letters = "ABCDEFGHIJKLNMOPQRSTUVWXYZ";
-        public static final int numberLetters[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
-                19, 20, 21, 22, 23, 24, 25 };
+        // public static final int numberLetters[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
 
         public static String encrypt(String message, int shiftKey) {
             message = message.toUpperCase();
@@ -51,7 +50,8 @@ public class labs5 {
 
                 System.out.println("\nEncrypt msg : " + encrypt(message, key));
             }
-            message = null;
+            sc.nextLine();
+            message = "";
             key = 0;
             {
                 System.out.print("Enter the String for Encrpttion : ");

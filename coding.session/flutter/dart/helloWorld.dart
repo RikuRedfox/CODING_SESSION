e39bc34a38266e47a86b195a4816d1a2808c3b3f
RikuRedfox <miyajimaurukiora@gmail.com>
2022-12-24 17:06:30 +0800

    codes commit

---
+++ coding.session/flutter/dart/helloWorld.dart
@@ -0,0 +1,64 @@
+import 'dart:io';
+
+main() {
+  {
+    /*
+    // stdout.write('What is your name? ');
+    // var name = stdin.readLineSync();
+    // // print('Your name is $name');
+    // stdout.write('Your name is $name');
+    */
+  }
+  {
+    /*
+    int amount = 1000;
+    var price = 200;
+
+    stdout.write('Amount: $amount & Price: $price \n');
+
+    double disAmount = 50;
+    var disPrice = 25;
+
+    stdout.write('Discount Amount: $disAmount & Discount Price: $disPrice \n');
+
+    String firstName = 'Andrew Jules';
+    var lastName = 'Miyajima';
+    stdout.write('First Name: $firstName & Last Name: $lastName\n');
+
+    dynamic hellowWorld = 'Hello World!';
+    stdout.writeln('$hellowWorld');
+
+    // declaration
+    dynamic var1 = 4.0e5 + 39;
+    var var2 = 74.21;
+
+    stdout.writeln(var1 + var2);
+
+    var1 = 'String there';
+    // var2 = 'String here';
+    stdout.writeln('$var1 | $var2');
+    */
+  }
+  {
+    /*
+    String str1 = 'Single quote';
+    String str2 = "Double quote";
+    String str3 = 'It\'s easy to escape the string limiter';
+    String str4 = "It's even easier to use the other limiter";
+    print(str1);
+    print(str2);
+    print(str3);
+    print(str4);
+
+    // RAW String
+
+    String str5 = r'Raw string\n its ignoring the\n';
+    print(str5);
+    */
+  }
+  {
+    int age = 19;
+    String str = 'My age is $age';
+    print(str);
+  }
+}
